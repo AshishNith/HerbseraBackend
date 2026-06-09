e74180f0087c97018fed8943b0b0ad5e313338a6
AshishNith <ranjan.ashish9992@gmail.com>
2026-06-09 21:04:25 +0530

    chore: remove hardcoded frontend origin, use FRONTEND_URL environment variable

--- server.js
+++ server.js
@@ -40,8 +40,7 @@ app.use(morgan('dev')); // Logging
 // CORS configuration
 const allowedOrigins = [
   'http://localhost:8080',
-  'http://192.168.1.10:8080',
-  'https://herbsera.in'
+  'http://192.168.1.10:8080'
 ];
 if (process.env.FRONTEND_URL) {
   const formattedUrl = process.env.FRONTEND_URL.replace(/\/$/, '');
