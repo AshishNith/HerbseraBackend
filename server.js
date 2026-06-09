5c7f0d3c385b9e71a1463a670ef9950d4038904b
AshishNith <ranjan.ashish9992@gmail.com>
2026-06-09 21:02:51 +0530

    fix(cors): allow origin https://herbsera.in

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use(morgan('dev')); // Logging
 const allowedOrigins = [
   'http://localhost:8080',
   'http://192.168.1.10:8080',
+  'https://herbsera.in'
 ];
 if (process.env.FRONTEND_URL) {
   const formattedUrl = process.env.FRONTEND_URL.replace(/\/$/, '');
